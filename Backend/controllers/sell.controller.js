61e709f059e564f1b227ca79b628292f17e85963
Anmolzixi <Anmolrajputzixisoft@gmail.com>
2023-10-12 17:33:13 +0530

    create update api done

--- Backend/controllers/sell.controller.js
+++ Backend/controllers/sell.controller.js
@@ -2,10 +2,16 @@ const connection = require('../database/mysqldb')
 
 function sellProperty(req, res) {
 
-    const { user_id, property_category_select,landPrice, mobile_number, full_address, state, city, pincode, landmark, owenership, cost_per_squre_fit, size_of_land, desciption, ownerName, Survey_no, Land_Facing } = req.body
+    const { user_id, property_category_select, landPrice, mobile_number, full_address, state, city, pincode, landmark, owenership, cost_per_squre_fit, size_of_land, desciption, ownerName, Survey_no, Land_Facing } = req.body
     const { image } = req.files
+    // const imageArray = []
+    // image.forEach((element) => {
+    //     imageArray.push(element.filename)
+    // })
     const file = image[0].filename
-
+    //     const imagestring = JSON.stringify(imageArray);
+    //     console.log(imagestring,'----');
+    // return
     const sql = `INSERT INTO test.tbl_sell_property (user_id, property_category_select, mobile_number, full_address, state, city, pincode, landmark, owenership, cost_per_squre_fit,landPrice, size_of_land, desciption,ownerName,Survey_no,Land_Facing,images) VALUES (?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?, ?,?,?,?,? ,?)`;
     const values = [
         user_id,
@@ -93,8 +99,50 @@ function getPropertyById(req, res) {
     })
 }
 
+function updateProperty(req, res) {
+    const { user_id, property_category_select, landPrice, mobile_number, full_address, state, city, pincode, landmark, owenership, cost_per_squre_fit, size_of_land, desciption, ownerName, Survey_no, Land_Facing } = req.body;
+    // const { image } = req.files;
+    // console.log(image,'image');
+    // const file = image[0].filename;
+    // console.log(file);
+    const propertyId = req.params.propertyId; // Assuming you pass the property ID as a parameter in the URL.
 
+    const sql = 'UPDATE test.tbl_sell_property SET  property_category_select="' + property_category_select + '", mobile_number="' + mobile_number + '", full_address="' + full_address + '", state="' + state + '", city="' + city + '", pincode="' + pincode + '", landmark="' + landmark + '", owenership="' + owenership + '", cost_per_squre_fit="' + cost_per_squre_fit + '", landPrice="' + landPrice + '", size_of_land="' + size_of_land + '", desciption="' + desciption + '", ownerName="' + ownerName + '", Survey_no="' + Survey_no + '", Land_Facing="' + Land_Facing + '" WHERE id="' + propertyId + '" AND  user_id="' + user_id + '"'
 
+    connection.query(sql, (err, result) => {
+        console.log(result, 'result');
+        if (err) {
+            console.error('Database update error: ' + err.message);
+            res.status(500).json({ error: 'Error updating data in the database' });
+        } else {
+            console.log('Data updated in the database.');
+            res.status(200).json({ message: 'Property Updated' });
+        }
+    });
+}
+
+function deleteProperty(req, res) {
+    const { propertyId, user_id } = req.body;
+    const checkQuery = 'SELECT id FROM test.tbl_sell_property WHERE id = "' + propertyId + '" AND user_id="' + user_id + '"';
+    connection.query(checkQuery, (checkErr, checkResult) => {
+        if (checkErr) {
+            return res.send({ error: checkErr });
+        }
+
+        if (checkResult.length === 0) {
+            return res.send({ error: 'Property not found' });
+        }
+
+        const deleteQuery = 'DELETE FROM test.tbl_sell_property WHERE id = "' + propertyId + '" AND user_id="' + user_id + '"';
+        connection.query(deleteQuery, (err, result) => {
+            if (err) {
+                return res.send({ error: err });
+            }
+
+            return res.send({ message: 'Property deleted successfully' });
+        });
+    });
+}
 
 function sortlist(req, res) {
     const { property_id, user_id, status } = req.body
@@ -145,7 +193,7 @@ function getsortlist(req, res) {
             return res.status(500).json({ error: "Internal Server Error" });
         } else {
             result.forEach(element => {
-                element.images = `http://localhost:5500/Backend/public/${element.images}`
+                element.images = `http://192.168.29.179:5500/Backend/public/${element.images}`
             });
             return res.send({ data: result })
         }
@@ -167,12 +215,23 @@ function buyInfo(req, res) {
     })
 }
 
-
 function getsortlistByID(req, res) {
     const { property_id, user_id } = req.body
-    connection.query('select * from test.tbl_sortlist where property_id=? and user_id=?', [property_id, user_id], (err, result) => {
+    connection.query('select * from test.tbl_sortlist where property_id="' + property_id + '" and user_id="' + user_id + '"', (err, result) => {
         return res.send({ data: result })
     })
 
 }
-module.exports = { sellProperty, getProperty, getPropertyById, sortlist, getsortlist, buyInfo, getsortlistByID }
+
+function sold_property(req, res) {
+    const { propertyId, user_id, sold_status } = req.body
+    connection.query('UPDATE test.tbl_sell_property SET sold = "' + sold_status + '" WHERE user_id = "' + user_id + '" AND id = "' + propertyId + '"', (err, updateResult) => {
+        if (err) {
+            console.log(err, '698');
+            return res.status(500).json({ error: 'Failed to update status' });
+        }
+        return res.status(200).json({ message: 'sold status updated' });
+    })
+}
+module.exports = { sellProperty, getProperty, getPropertyById, sortlist, getsortlist, buyInfo, getsortlistByID, updateProperty, deleteProperty, sold_property }
+
--- Backend/routes/auth/sell.route.js
+++ Backend/routes/auth/sell.route.js
@@ -1,15 +1,18 @@
 var express = require('express')
 const sellRouter = express.Router();
 
-const { singleUpload,multiplepUload } = require('../../middleware/multer');
+const { singleUpload, multiplepUload } = require('../../middleware/multer');
 
-const { sellProperty, getProperty, getsortlist,getsortlistByID,getPropertyById ,sortlist,buyInfo} = require("../../controllers/sell.controller")
+const { sellProperty, getProperty, getsortlist, getsortlistByID, deleteProperty, getPropertyById, sortlist, buyInfo, updateProperty, sold_property } = require("../../controllers/sell.controller")
 sellRouter.post('/sellProperty', multiplepUload, sellProperty);
 sellRouter.post('/getproperty', getProperty)
 sellRouter.post('/getPropertyById', getPropertyById)
-sellRouter.post('/sortlist',sortlist)
-sellRouter.post('/getsortlist',getsortlist)
-sellRouter.post('/buy',buyInfo)
-sellRouter.post('/getsortlistByID',getsortlistByID)
+sellRouter.post('/sortlist', sortlist)
+sellRouter.post('/getsortlist', getsortlist)
+sellRouter.post('/buy', buyInfo)
+sellRouter.post('/getsortlistByID', getsortlistByID)
+sellRouter.post('/updateProperty/:propertyId', singleUpload, updateProperty)
+sellRouter.post('/delete_proprty', deleteProperty)
+sellRouter.post('/sold_property', sold_property)
 
 module.exports = sellRouter;
